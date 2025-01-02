13759444d7c08c3bb40044cc2347b8d977181eb6
Kyky Gaming <mesmacquetimothey@gmail.com>
2025-01-02 16:38:24 +0100

    added some links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Restaurant from "./pages/pageRestaurant/Restaurant";
-import Login from "./pages/login/Login";
 import NavBar from "./components/NavBar/NavBar";
 function App() {
   return (
     <>
       <NavBar />
       <Outlet />
-      <NavBar />
-      <Login />
-      <Restaurant />
     </>
   );
 }
