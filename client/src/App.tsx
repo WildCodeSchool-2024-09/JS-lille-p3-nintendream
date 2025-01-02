77d73bb701d68db004ccfbf6797bb13f07e58655
ALICIA <alicia.magro@gmail.com>
2025-01-02 11:17:41 +0100

    created a new branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Billetterie from "./pages/billetterie/Billetterie";
 import NavBar from "./components/NavBar/NavBar";
+import Hotels from "./pages/hotels/hotels";
 
 function App() {
   return (
     <>
       <NavBar />;
       <Billetterie />
+      <Hotels />
     </>
   );
 }
---
+++ client/src/pages/hotels/hotels.tsx
@@ -0,0 +1,10 @@
+import "./hotels.css";
+
+function hotels () {
+    return (
+        <> <h1>holà, que tal ?</h1>
+        </>
+    )
+}
+
+export default hotels
