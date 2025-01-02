8a372176ec28c2bd0b114c3e65015749cfd4c525
ALICIA <alicia.magro@gmail.com>
2025-01-02 16:43:57 +0100

    the main hotel page is done

--- client/src/pages/hotels/hotels.tsx
+++ client/src/pages/hotels/hotels.tsx
@@ -30,29 +30,42 @@ const hotels: hotelsProps[] = [
       "Venez rencontrer Mario et ses amis dans un hôtel coloré et chaleureux !",
   },
 ];
+
 function HotelsPage() {
   return (
     <>
       <main className="hotels-container">
-        <h1>Nos hôtels</h1>
-        <section className="hotels-description-container">
-          <p className="hotels-description">
-            {" "}
-            Découvez notre sélection d'hôtels ! Vous cherchez un hôtel familial
-            pour faire rêver votre enfant ? Ou bien préférez-vous un hôtel plus
-            luxueux pour vous détendre après une journée de visite ? Ou encore
-            un hôtel économique pour profiter de votre séjour sans vous ruiner ?
-            À Nintendream, nous avon tout ce qu'il vous faut !
-          </p>
-        </section>
-        {hotels.map((hotel: hotelsProps) => (
-          <section key={hotel.name} className="hotel-card">
-            <img src={hotel.img} alt="hotel" className="hotel-img" />
-            <h2>{hotel.name}</h2>
-            <p>{hotel.description}</p>
-            <p>{hotel.distance} m du parc</p>
-          </section>
-        ))}
+        <h1 className="main-title-hotel">Nos hôtels</h1>
+        <p className="hotels-description">
+          {" "}
+          Découvez notre sélection d'hôtels ! Vous cherchez un hôtel familial
+          pour faire rêver votre enfant ? Ou bien préférez-vous un hôtel plus
+          luxueux pour vous détendre après une journée de visite ? Ou encore un
+          hôtel économique pour profiter de votre séjour sans vous ruiner ? À
+          Nintendream, nous avon tout ce qu'il vous faut !
+        </p>
+
+        <div className="hotel-card-container">
+          {hotels.map((hotel: hotelsProps) => (
+            <section key={hotel.name} className="hotel-card">
+              <img src={hotel.img} alt="hotel" className="hotel-card-img" />
+              <h2 className="hotel-card-name">{hotel.name}</h2>
+              <hr className="first-line" />
+              <div className="hotel-card-rows">
+                <div className="hotel-card-distance-first-row">
+                  <p className="hotel-card-distance">{hotel.distance}</p>
+                </div>
+                <div className="hotel-card-distance-second-row">
+                  <hr className="second-line" /> <p>m du parc</p>
+                </div>
+              </div>
+              <p className="hotel-card-description">{hotel.description}</p>
+              <button type="button" className="hotel-card-button">
+                Voir l'hôtel
+              </button>
+            </section>
+          ))}
+        </div>
       </main>
     </>
   );
