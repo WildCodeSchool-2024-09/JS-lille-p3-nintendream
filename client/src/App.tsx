3e388d39e633c140a9db0a505fa2949380a755f7
ALICIA <alicia.magro@gmail.com>
2025-01-03 14:55:34 +0100

    responsive is done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Restaurant from "./pages/pageRestaurant/Restaurant";
-import Login from "./pages/login/Login";
 import NavBar from "./components/NavBar/NavBar";
+import Login from "./pages/login/Login";
+import Restaurant from "./pages/pageRestaurant/Restaurant";
 
 function App() {
   return (
