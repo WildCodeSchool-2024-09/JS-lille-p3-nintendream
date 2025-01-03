e420b2ec3848627aea8c653d606cca52262154ba
ALICIA <alicia.magro@gmail.com>
2025-01-03 15:22:37 +0100

    fixed a few problemes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import Login from "./pages/login/Login";
-import Restaurant from "./pages/pageRestaurant/Restaurant";
+import HotelsPage from "./pages/hotels/hotels";
+
 
 function App() {
   return (
     <>
       <NavBar />
       <Outlet />
-      <NavBar />
-      <Login />
-      <Restaurant />
+      <HotelsPage />
+      
     </>
   );
 }
