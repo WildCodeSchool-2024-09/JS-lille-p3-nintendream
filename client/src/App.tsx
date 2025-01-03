7e7567196bae13b75dee64bb4b2b2a6a2af26986
ALICIA <alicia.magro@gmail.com>
2025-01-03 17:25:08 +0100

    working on the individual hotel page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import HotelDetails from "/src/pages/hotels/hotelDetails.tsx";
+import HotelDetails from "./pages/hotels/hotelDetails";
 
 function App() {
   return (
