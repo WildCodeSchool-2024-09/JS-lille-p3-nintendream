75ab00c405f4016a99320cd4938625d8d618d169
Thomas Lamisse <tom.lamisse@gmail.com>
2025-01-17 11:36:03 +0100

    fixing classname

--- client/src/pages/profile/profile.tsx
+++ client/src/pages/profile/profile.tsx
@@ -8,16 +8,16 @@ function Profile() {
           <div>
             <h1 className="title-profile">Vos informations</h1>
           </div>
-          <div className="texte-profile">
-            Voici les informations que vous avez enregistrés depuis la création
+          <div className="text-profile">
+            Voici les informations que vous avez enregistrées depuis la création
             de votre compte Nintendream :
           </div>
           <div className="email-profile">E-mail :</div>
-          <div className="prenom-profile">Prénom :</div>
-          <div className="nom-profile">Nom :</div>
+          <div className="firstname-profile">Prénom :</div>
+          <div className="name-profile">Nom :</div>
           <div className="age-profile">Age :</div>
           <div className="ending-profile">
-            A très vite dans notre super parc Nitendream !
+            A très vite dans notre super parc Nintendream !
           </div>
         </section>
       </body>
