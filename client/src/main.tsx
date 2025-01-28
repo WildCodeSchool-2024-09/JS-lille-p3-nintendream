2d4849eb41c38b7ebc129c6553527dcbf0e2d30d
ALICIA <alicia.magro@gmail.com>
2025-01-28 09:47:00 +0100

    comitting so I can pull

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import EditAttractionForm from "./pages/admin//Attraction/EditAttractionForm.tsx";
 import Admin from "./pages/admin/Admin.tsx";
 import DeleteAttractionForm from "./pages/admin/Attraction/DeleteAttractionForm.tsx";
 import AttractionForm from "./pages/admin/Attraction/NewAttractionForm.tsx";
-import EditAttractionForm from "./pages/admin//Attraction/EditAttractionForm.tsx";
 import DeleteRestaurantForm from "./pages/admin/restaurant_admin/DeleteRestaurantForm.tsx";
 import EditRestaurantForm from "./pages/admin/restaurant_admin/EditRestaurantForm.tsx";
 import RestaurantForm from "./pages/admin/restaurant_admin/NewRestaurantForm.tsx";
@@ -26,8 +26,8 @@ import Login from "./pages/login/Login";
 import Restaurant from "./pages/pageRestaurant/Restaurant";
 import RestaurantDetails from "./pages/pageRestaurant/RestaurantDetails";
 import Profile from "./pages/profile/profile.tsx";
-import Reservation from "./pages/reservation/Reservation.tsx";
 import Register from "./pages/register/Register.tsx";
+import Reservation from "./pages/reservation/Reservation.tsx";
 
 /* ************************************************************************* */
 
@@ -37,49 +37,23 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "/", 
-        element: <Homepage /> 
-      },
-      { path: "/login", 
-        element: <Login /> 
-      },
-      { path: "/billetterie", 
-        element: <Billetterie /> 
-      },
-      { path: "/hotels", 
-        element: <HotelsPage /> 
-      },
-      { path: "/hotel/:id", 
-        element: <HotelDetails /> 
-      },
-      { path: "/admin", 
-        element: <Admin /> 
-      },
-      { path: "/profile", 
-        element: <Profile /> 
-      },
-      { path: "/reservation/:name", 
-        element: <Reservation /> 
-      },
-      { path: "/confirmation", 
-        element: <Confirmation /> 
-      },
+      { path: "/", element: <Homepage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/billetterie", element: <Billetterie /> },
+      { path: "/hotels", element: <HotelsPage /> },
+      { path: "/hotel/:id", element: <HotelDetails /> },
+      { path: "/admin", element: <Admin /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/reservation/:name", element: <Reservation /> },
+      { path: "/confirmation", element: <Confirmation /> },
       {
         path: "/restaurants",
         element: <Restaurant />,
       },
-      { path: "/restaurant/:id", 
-        element: <RestaurantDetails /> 
-      },
-      { path: "/evenements", 
-        element: <Evenements /> 
-      },
-      { path: "/evenements/:id", 
-        element: <EvenementDetails /> 
-      },
-      { path: "/*", 
-        element: <ErrorPage /> 
-      },
+      { path: "/restaurant/:id", element: <RestaurantDetails /> },
+      { path: "/evenements", element: <Evenements /> },
+      { path: "/evenements/:id", element: <EvenementDetails /> },
+      { path: "/*", element: <ErrorPage /> },
       {
         path: "/register",
         element: <Register />,
