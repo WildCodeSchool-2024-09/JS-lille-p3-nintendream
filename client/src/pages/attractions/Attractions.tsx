99807adbf4626d9007d4ffdaf47a0a8bb0a7bf75
Kyky Gaming <mesmacquetimothey@gmail.com>
2025-02-07 12:30:01 +0100

    made the filters for the rides!!

--- client/src/pages/attractions/Attractions.tsx
+++ client/src/pages/attractions/Attractions.tsx
@@ -18,7 +18,7 @@ interface Attractions {
 
 function Attractions() {
   const [attractions, setAttractions] = useState([] as Attractions[]);
-
+  const [attractionsFilter, setAttractionsFilter] = useState("Tout");
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/attractions`)
       .then((response) => response.json())
@@ -28,7 +28,7 @@ function Attractions() {
   });
 
   return (
-    <>
+    <main>
       <div className="image-background-attraction">
         <img
           className="background-attraction"
@@ -36,23 +36,67 @@ function Attractions() {
           alt="attraction"
         />
       </div>
+      <div className="attraction-flex-button-p">
+        <p>Filtres</p>
+        <div className="attraction-flex-button">
+          <button
+            type="button"
+            onClick={() => {
+              setAttractionsFilter("Tout");
+            }}
+            className="filter-button-attraction"
+          >
+            Tout
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setAttractionsFilter("ENFANT");
+            }}
+            className="filter-button-attraction"
+          >
+            Enfant
+          </button>
+          <button
+            type="button"
+            onClick={() => setAttractionsFilter("FAMILLE")}
+            className="filter-button-attraction"
+          >
+            Famille
+          </button>
+          <button
+            type="button"
+            onClick={() => setAttractionsFilter("SENSATION FORTE")}
+            className="filter-button-attraction"
+          >
+            Sensation forte
+          </button>
+        </div>
+      </div>
       <section className="container-card-attractions">
         {attractions.map((attraction) => (
-          <div key={attraction.id} className="card-link-attraction">
-            <Link to={`/attraction/${attraction.id}`}>
-              <img
-                className="img-attraction"
-                src={attraction.img}
-                alt="attraction"
-              />
-              <div className="texte-card-attraction">
-                <h1>{attraction.name}</h1>
-                <p>{attraction.description}</p>
-                <p>Type : {attraction.type}</p>
-                <p>Hauteur minimum : {attraction.min_height}</p>
+          <>
+            {attraction.type === attractionsFilter ||
+            attractionsFilter === "Tout" ? (
+              <div key={attraction.id} className="card-link-attraction">
+                <Link to={`/attraction/${attraction.id}`}>
+                  <img
+                    className="img-attraction"
+                    src={attraction.img}
+                    alt="attraction"
+                  />
+                  <div className="texte-card-attraction">
+                    <h1>{attraction.name}</h1>
+                    <p>{attraction.description}</p>
+                    <p>Type : {attraction.type}</p>
+                    <p>Hauteur minimum : {attraction.min_height}</p>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
+            ) : (
+              ""
+            )}
+          </>
         ))}
       </section>
 
@@ -61,7 +105,7 @@ function Attractions() {
         src="/public/image/attractions/MapNintendDream.jpeg"
         alt="Map"
       />
-    </>
+    </main>
   );
 }
 
