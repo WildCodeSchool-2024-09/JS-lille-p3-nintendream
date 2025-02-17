e002fde65cacd65d2f5870b0511e49597bbae084
Thomas Lamisse <tom.lamisse@gmail.com>
2025-02-17 12:15:34 +0100

    responsive restau

---
+++ client/src/components/restaurant/RestaurantDeleteForm.tsx
@@ -0,0 +1,32 @@
+import type { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+import "./RestaurantForm.css";
+
+type RestaurantDeleteFormProps = {
+  id: number;
+  children: ReactNode;
+};
+
+function RestaurantDeleteForm({ id, children }: RestaurantDeleteFormProps) {
+  const navigate = useNavigate();
+  return (
+    <form
+      onSubmit={(restaurant) => {
+        restaurant.preventDefault();
+        fetch(`${import.meta.env.VITE_API_URL}/api/restaurant/${id}`, {
+          method: "delete",
+        }).then((response) => {
+          if (response.status === 204) {
+            navigate("/admin");
+          }
+        });
+      }}
+    >
+      <button type="submit" className="admin-form-restaurant-button">
+        {children}
+      </button>
+    </form>
+  );
+}
+
+export default RestaurantDeleteForm;
